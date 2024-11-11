2d0c73261abe95893da5b36814ec01290a19a214
baoispro <nguyenlegiabao810@gmail.com>
2024-11-11 14:50:40 +0700

    complete launch screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, ImageBackground,Image,Pressable } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <ImageBackground source={require('@/assets/images/background.png')} resizeMode="cover" style={{flex:1,width:'100%',justifyContent:'space-evenly',alignItems:'center'}}>
+          <Image source={require('@/assets/images/logoBackground.png')} style={{width:'27%', height:'8%', marginBottom:200}}/>
+          <View>
+            <Text style={{color:'white', fontWeight:'bold',fontSize:40}}>Your music</Text>
+            <Text style={{color:'white', fontWeight:'bold',fontSize:40}}>Your artists</Text>
+          </View>
+          <View style={{width:'90%', height:'12%', gap: 30}}>
+            <Pressable style={{backgroundColor:'#171A1F',borderRadius: 26, width: '100%',height:'50%', alignItems:'center', justifyContent:'center'}}>
+              <Text style={{color:'white', fontSize: 18}}>Create an account</Text>
+            </Pressable>
+            <Pressable style={{backgroundColor:'#F7F1FE',borderRadius: 26, width: '100%',height:'50%', alignItems:'center', justifyContent:'center'}}>
+              <Text style={{color:'#9D45EF', fontSize: 18}}>I already have an account</Text>
+            </Pressable>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+
+});
